88e3014ca7904236ffa4c57f46f8f12282dcc0fe
ellen3128 <hyunpark.ellen@gmail.com>
2023-08-10 13:37:55 -0400

    Create function added

--- express-cat/controllers/cats.js
+++ express-cat/controllers/cats.js
@@ -1,10 +1,21 @@
 const Cat = require("../models/cat");
 
 module.exports = {
-    new: newCat
+    new: newCat,
+    create
 };
 
 
 function newCat(req, res) {
     res.render("cats/new", {title: "Add Cat", errorMsg: ""});
+}
+
+async function create(req, res) {
+    const catData = {...req.body}
+    try {
+        const createdCat = await Cat.create(catData); 
+        res.redirect("/cats/" + createdCat._id);
+    } catch (err) {
+        res.render("cats/new", {errorMsg: err.message});
+    }
 }
--- express-cat/routes/cats.js
+++ express-cat/routes/cats.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const catCtrl = require("../controllers/cats");
 
+// New 
 router.get("/new", catCtrl.new);
+
+// Create 
+router.post("/", catCtrl.create);
 
 module.exports = router;
